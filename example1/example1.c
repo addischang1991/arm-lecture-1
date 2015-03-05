830da268173a13f2405e44f6f50c6d1326845973
addischang1991 <addischang1991@livemail.tw>
2015-03-05 15:00:40 +0800

    Modify the return

--- example1/example1.c
+++ example1/example1.c
@@ -2,11 +2,17 @@ int add(int, int);
 
 int main()
 {
+	// Declare the type of vriables 
 	int a, b, c;
 	a=10;
 	b=12;
+
+	// Call function "add"
+
 	c=add(a,b);
-	return 0;
+
+	// Set "c" as answer return
+	return c;
 }
 
 int add(int a, int b)
