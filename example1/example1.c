3b89f7f5517c3fb48ad5b6a9a6c9ba66c124da8d
addischang1991 <addischang1991@livemail.tw>
2015-03-08 15:47:15 +0800

    Remove ELF

--- example1/example1.c
+++ example1/example1.c
@@ -1,17 +1,11 @@
 int add(int, int);
 
 int main()
-{
-	// Declare the type of vriables 
+{ 
 	int a, b, c;
 	a=10;
 	b=12;
-
-	// Call function "add"
-
 	c=add(a,b);
-
-	// Set "c" as answer return
 	return c;
 }
 
